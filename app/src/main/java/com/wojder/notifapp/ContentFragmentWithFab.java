6efb0a1971bd69cdc2526b5902a00112aa2731f8
wojder <wojdera@demioorg.pl>
2016-02-16 17:59:23 +0100

    Created notification app implementing Intents, custom notification layout and downloading pdf file from web

---
+++ app/src/main/java/com/wojder/notifapp/ContentFragmentWithFab.java
@@ -0,0 +1,52 @@
+package com.wojder.notifapp;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.design.widget.FloatingActionButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContentFragmentWithFab extends Fragment implements View.OnClickListener {
+
+    private FloatingActionButton fab = null;
+
+    public ContentFragmentWithFab() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+    // Inflate the layout for this fragment
+    View result = inflater.inflate(R.layout.content_fragment_with_fab, container, false);
+//    result.findViewById(R.id.fab_button).setOnClickListener(this);
+
+        fab = (FloatingActionButton) result.findViewById(R.id.fab_button);
+        fab.setOnClickListener(this);
+
+
+
+    return (result);
+//                inflater.inflate(R.layout.content_fragment_with_fab, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i=new Intent(getActivity(), Downloader.class);
+
+        i.setDataAndType(Uri.parse("http://commonsware.com/Android/excerpt.pdf"),
+                "application/pdf");
+
+        getActivity().startService(i);
+        getActivity().finish();
+    }
+}
---
+++ app/src/main/java/com/wojder/notifapp/Downloader.java
@@ -0,0 +1,117 @@
+package com.wojder.notifapp;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by wojder on 15.02.16.
+ */
+public class Downloader extends IntentService {
+
+
+    private static final int NOTIFY_ID = 1337;
+
+    public Downloader() {
+        super("Downloader");
+    }
+    @Override
+    public void onHandleIntent(Intent i) {
+        try {
+            File root=
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+            root.mkdirs();
+
+            File output=new File(root, i.getData().getLastPathSegment());
+
+            if (output.exists()) {
+                output.delete();
+            }
+
+            URL url=new URL(i.getData().toString());
+            HttpURLConnection c=(HttpURLConnection)url.openConnection();
+            FileOutputStream fos=new FileOutputStream(output.getPath());
+            BufferedOutputStream out=new BufferedOutputStream(fos);
+
+            try {
+                InputStream in=c.getInputStream();
+                byte[] buffer=new byte[8192];
+                int len=0;
+
+                while ((len=in.read(buffer)) >= 0) {
+                    out.write(buffer, 0, len);
+                }
+
+                out.flush();
+            }
+            finally {
+                fos.getFD().sync();
+                out.close();
+                c.disconnect();
+            }
+
+            launchNotification(i, output, null);
+        }
+        catch (IOException e2) {
+            launchNotification(i, null, e2);
+        }
+    }
+
+    private void launchNotification (Intent inbound, File output, Exception e){
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this);
+        NotificationManager mgr = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        NotificationCompat.InboxStyle large = new NotificationCompat.InboxStyle(builder);
+
+        mgr.notify(NOTIFY_ID, large.setSummaryText("Test summary")
+        .addLine("First line")
+        .addLine("Second Line").addLine("4 line").addLine("5th line")
+        .addLine("Third line").build());
+
+        builder.setAutoCancel(true).setDefaults(Notification.DEFAULT_ALL);
+
+        if (e == null) {
+            builder.setContentTitle(getString(R.string.download_complete))
+                    .setContentText(getString(R.string.fun))
+                    .setSmallIcon(android.R.drawable.stat_sys_download_done)
+                    .addAction(android.R.drawable.ic_media_play, "Play", buildPendingIntent(Settings.ACTION_SETTINGS))
+                    .setContentIntent(buildPendingIntent(Settings.ACTION_SECURITY_SETTINGS))
+                    .setTicker(getString(R.string.download_complete));
+
+            Intent outbound=new Intent(Intent.ACTION_VIEW);
+
+            outbound.setDataAndType(Uri.fromFile(output), inbound.getType());
+
+            builder.setContentIntent(PendingIntent.getActivity(this, 0, outbound, 0));
+        }
+        else {
+            builder.setContentTitle(getString(R.string.exception))
+                    .setContentText(e.getMessage())
+                    .setSmallIcon(android.R.drawable.stat_notify_error)
+                    .setTicker(getString(R.string.exception));
+        }
+
+        mgr.notify(NOTIFY_ID, builder.build());
+    }
+
+    private PendingIntent buildPendingIntent(String go) {
+        Intent intent = new Intent(go);
+
+        return (PendingIntent.getActivity(this, 0, intent, 0));
+    }
+}
